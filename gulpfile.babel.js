a6e43f8e3232ff4543bec3489005407c8974b68c
suttang <suttang@gmail.com>
2016-08-27 00:04:36 +0900

    gulp scss

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,10 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const minifyCss = require('gulp-minify-css');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
 const electron = require('electron-connect').server.create();
+
 
 gulp.task('serve', () => {
   // Start browser process
@@ -13,4 +18,34 @@ gulp.task('serve', () => {
 });
 
 
-gulp.task('default', ['serve']);
+gulp.task('reload:browser', () => {
+  electron.restart();
+});
+
+
+gulp.task('reload:renderer', () => {
+  // Reload renderer process
+  electron.reload();
+});
+
+
+gulp.task('sass', (done) => {
+  gulp.src('./assets/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./assets/css/'))
+    .pipe(minifyCss({ keepSpecialComments: 0 }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./assets/css/'))
+    .on('end', done);
+});
+
+
+gulp.task('watch', ['sass', 'serve'], () => {
+  gulp.watch('./assets/scss/**/*.scss', ['scss', 'reload:browser']);
+});
+
+
+gulp.task('default', ['watch']);
