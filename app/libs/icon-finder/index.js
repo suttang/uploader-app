1714d41bffd46fdec448d8ceda52bf9f094bc743
suttang <suttang@gmail.com>
2016-08-30 16:50:58 +0900

    調整

---
+++ app/libs/icon-finder/index.js
@@ -0,0 +1,28 @@
+const extIconMap = {
+  ai: 'icon-ai.png',
+  doc: 'icon-doc.png',
+  docx: 'icon-doc.png',
+  htm: 'icon-html.png',
+  html: 'icon-html.png',
+  jpg: 'icon-jpg.png',
+  mp3: 'icon-mp3.png',
+  pdf: 'icon-pdf.png',
+  png: 'icon-png.png',
+  ppt: 'icon-ppt.png',
+  psd: 'icon-psd.png',
+  txt: 'icon-txt.png',
+  xls: 'icon-xls.png',
+  xml: 'icon-xml.png',
+  zip: 'icon-zip.png',
+};
+const fallbackIcon = extIconMap.txt;
+
+module.exports = function findIcon(filename) {
+  const filenames = filename.split('.');
+  const extension = filenames[filenames.length - 1].toLowerCase();
+  const icon = extIconMap[extension];
+  if (icon) {
+    return icon;
+  }
+  return extIconMap[fallbackIcon];
+};
